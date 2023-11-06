6c81eb8bb9a18295a6be3c3c0b483845f6dcab13
SheepDoctor <12211111@mail.sustech.edu.cn>
2023-11-06 16:30:05 +0800

    0.2.4 Runnable version to load danmu

---
+++ src/version_1/Likes.java
@@ -0,0 +1,26 @@
+package version_1;
+
+import utils.Database;
+import version_1.Loader;
+
+import java.util.Properties;
+
+public class Likes
+{
+    public static void main(String[] args)
+    {
+        Properties prop = new Properties();
+        prop.put("host", "localhost");
+        prop.put("user", "postgres");
+        prop.put("password", "123456");
+        prop.put("database", "Project");
+
+        String file_path = "source_file/videos.csv";
+        String[] queue = {"String", "Skip", "Skip", "Skip", "Skip", "Skip", "Skip", "Skip", "Skip", "Skip", "List", "Skip", "Skip", "Skip"};
+        String sql = "insert into videos(bv,mid) values(?,?)";
+        Database database = new Database(prop);
+
+        Loader loader = new Loader();
+        loader.write_data(file_path, queue, database, sql, false);
+    }
+}
---
+++ src/version_1/Videos.java
@@ -0,0 +1,26 @@
+package version_1;
+
+import utils.Database;
+import version_1.Loader;
+
+import java.util.Properties;
+
+public class Videos
+{
+    public static void main(String[] args)
+    {
+        Properties prop = new Properties();
+        prop.put("host", "localhost");
+        prop.put("user", "postgres");
+        prop.put("password", "123456");
+        prop.put("database", "Project");
+
+        String file_path = "source_file/videos.csv";
+        String[] queue = {"String", "String", "Long", "Skip", "Time", "Skip", "Time", "Int", "String", "Skip", "Skip", "Skip", "Skip", "Skip"};
+        String sql = "insert into videos(bv, title, owner_id, commit_time, public_time, duration, description) values(?,?,?,?,?,?,?)";
+        Database database = new Database(prop);
+
+        Loader loader = new Loader();
+        loader.write_data(file_path, queue, database, sql, false);
+    }
+}
