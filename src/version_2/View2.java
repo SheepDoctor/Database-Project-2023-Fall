dac7f2344135d4ad819e8d88fda23730433eb889
xzt <1723716131@qq.com>
2023-11-09 21:56:30 +0800

    0.4.1

--- src/version_2/View2.java
+++ src/version_2/View2.java
@@ -3,19 +3,23 @@ package version_2;
 import utils.Database;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class View2
 {
     public static void main(String[] args)
     {
+        ExecutorService executorService= Executors.newCachedThreadPool();
+
         Properties prop = new Properties();
         prop.put("host", "localhost");
         prop.put("user", "postgres");
         prop.put("password", "123abc");
         prop.put("database", "Project");
 
-        //String file_path = "source_file/videos.csv";
-        String file_path = "source_file/draft.csv";
+        String file_path = "source_file/videos.csv";
+        //String file_path = "source_file/draft.csv";
         String[] queue = {
                 "Long",
                 "Skip",
@@ -35,6 +39,7 @@ public class View2
         Database database = new Database(prop);
 
         ViewLoader2 loader = new ViewLoader2();
-        loader.write_data(file_path, queue, database, sql, false, false);
+        loader.write_data(file_path, queue, database, sql, false, false,executorService);
+
     }
 }
