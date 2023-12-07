7e3da8e4639661a2ef7781d8c2223c44b15929ff
han <981659021@qq.com>
2023-12-07 21:08:36 +0800

    1.0.2 何：新增了接口部分的中文翻译

--- sustc-api/src/main/java/io/sustc/service/VideoService.java
+++ sustc-api/src/main/java/io/sustc/service/VideoService.java
@@ -10,223 +10,228 @@ import java.util.Set;
 public interface VideoService {
 
     /**
-     * Posts a video. Its commit time shall be {@link LocalDateTime#now()}.
+     * 发布一个视频。其提交时间应该是 {@link LocalDateTime#now()}。
      *
-     * @param auth the current user's authentication information
-     * @param req  the video's information
-     * @return the video's {@code bv}
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param req  视频的信息
+     * @return 视频的 {@code bv}
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>{@code req} is invalid
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>{@code req} 无效
      *     <ul>
-     *       <li>{@code title} is null or empty</li>
-     *       <li>there is another video with same {@code title} and same user</li>
-     *       <li>{@code duration} is less than 10 (so that no chunk can be divided)</li>
-     *       <li>{@code publicTime} is earlier than {@link LocalDateTime#now()}</li>
+     *       <li>{@code title} 为空或者空字符串</li>
+     *       <li>存在另一个同名且属于同一用户的视频</li>
+     *       <li>{@code duration} 小于10（这样就无法分割成片段）</li>
+     *       <li>{@code publicTime} 早于 {@link LocalDateTime#now()}</li>
      *     </ul>
      *   </li>
      * </ul>
-     * If any of the corner case happened, {@code null} shall be returned.
+     * 如果出现任何边界情况，应该返回 {@code null}。
      */
     String postVideo(AuthInfo auth, PostVideoReq req);
 
     /**
-     * Deletes a video.
-     * This operation can be performed by the video owner or a superuser.
+     * 删除一个视频。
+     * 这个操作可以由视频所有者或超级用户执行。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @return success or not
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv}
+     * @return 是否成功
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>{@code auth} is not the owner of the video nor a superuser</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>{@code auth} 既不是视频所有者也不是超级用户</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果出现任何边界情况，应该返回 {@code false}。
      */
     boolean deleteVideo(AuthInfo auth, String bv);
 
     /**
-     * Updates the video's information.
-     * Only the owner of the video can update the video's information.
-     * If the video was reviewed before, a new review for the updated video is required.
-     * The duration shall not be modified and therefore the likes, favorites and danmus are not required to update.
+     * 更新视频信息。
+     * 只有视频所有者可以更新视频信息。
+     * 如果视频之前已经被审核，那么更新后的视频需要重新审核。
+     * 视频时长不应修改，因此不需要更新点赞、收藏和弹幕。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @param req  the new video information
-     * @return {@code true} if the video needs to be re-reviewed (was reviewed before), {@code false} otherwise
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv}
+     * @param req  新的视频信息
+     * @return 如果视频需要重新审核（之前已经审核过），则返回 {@code true}，否则返回 {@code false}
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>{@code auth} is not the owner of the video</li>
-     *   <li>{@code req} is invalid, as stated in {@link io.sustc.service.VideoService#postVideo(AuthInfo, PostVideoReq)}</li>
-     *   <li>{@code duration} in {@code req} is changed compared to current one</li>
-     *   <li>{@code req} is not changed compared to current information</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>{@code auth} 不是视频所有者</li>
+     *   <li>{@code req} 无效，如 {@link io.sustc.service.VideoService#postVideo(AuthInfo, PostVideoReq)} 中所述</li>
+     *   <li>{@code req} 中的 {@code duration} 与当前视频时长不同</li>
+     *   <li>{@code req} 与当前信息相同，没有变化</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果出现任何边界情况，应该返回 {@code false}。
      */
     boolean updateVideoInfo(AuthInfo auth, String bv, PostVideoReq req);
 
     /**
-     * Search the videos by keywords (split by space).
-     * You should try to match the keywords case-insensitively in the following fields:
+     * 通过关键词（以空格分隔）搜索视频。
+     * 您应该尝试在以下字段中不区分大小写地匹配关键词：
      * <ol>
-     *   <li>title</li>
-     *   <li>description</li>
-     *   <li>owner name</li>
+     *   <li>标题</li>
+     *   <li>描述</li>
+     *   <li>所有者名称</li>
      * </ol>
      * <p>
-     * Sort the results by the relevance (sum up the number of keywords matched in the three fields).
+     * 根据相关性对结果进行排序（在这三个字段中匹配的关键词数量之和）。
      * <ul>
-     *   <li>If a keyword occurs multiple times, it should be counted more than once.</li>
+     *   <li>如果一个关键词出现多次，它应该被重复计数。</li>
      *   <li>
-     *     A character in these fields can only be counted once for each keyword
-     *     but can be counted for different keywords.
+     *     这些字段中的一个字符对于每个关键词只能计数一次
+     *     但可以对不同的关键词计数。
      *   </li>
-     *   <li>If two videos have the same relevance, sort them by the number of views.</li>
+     *   <li>如果两个视频的相关性相同，按照观看次数排序。</li>
      * </u
      * <p>
-     * Examples:
+     * 示例：
      * <ol>
      *   <li>
-     *     If the title is "1122" and the keywords are "11 12",
-     *     then the relevance in the title is 2 (one for "11" and one for "12").
+     *     如果标题是 "1122"，关键词是 "11 12"，
+     *     那么标题中的相关性是 2（"11" 一个和 "12" 一个）。
      *   </li>
      *   <li>
-     *     If the title is "111" and the keyword is "11",
-     *     then the relevance in the title is 1 (one for the occurrence of "11").
+     *     如果标题是 "111"，关键词是 "11"，
+     *     那么标题中的相关性是 1（"11" 出现一次）。
      *   </li>
      *   <li>
-     *     Consider a video with title "Java Tutorial", description "Basic to Advanced Java", owner name "John Doe".
-     *     If the search keywords are "Java Advanced",
-     *     then the relevance is 3 (one occurrence in the title and two in the description).
+     *     考虑一个视频，标题为 "Java Tutorial"，描述为 "Basic to Advanced Java"，所有者名为 "John Doe"。
+     *     如果搜索关键词是 "Java Advanced"，
+     *     那么相关性是 3（标题中出现一次，描述中出现两次）。
      *   </li>
      * </ol>
      * <p>
-     * Unreviewed or unpublished videos are only visible to superusers or the video owner.
+     * 未审核或未发布的视频只对超级用户或视频所有者可见。
      *
-     * @param auth     the current user's authentication information
-     * @param keywords the keywords to search, e.g. "sustech database final review"
-     * @param pageSize the page size, if there are less than {@code pageSize} videos, return all of them
-     * @param pageNum  the page number, starts from 1
-     * @return a list of video {@code bv}s
-     * @implNote If the requested page is empty, return an empty list
-     * @apiNote You may consider the following corner cases:
+     * @param auth     当前用户的认证信息
+     * @param keywords 搜索关键词，例如 "sustech database final review"
+     * @param pageSize 页面大小，如果视频少于 {@code pageSize}，则返回所有视频
+     * @param pageNum  页面号，从 1 开始
+     * @return 视频 {@code bv} 的列表
+     * @implNote 如果请求的页面为空，则返回一个空列表
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>{@code keywords} is null or empty</li>
-     *   <li>{@code pageSize} and/or {@code pageNum} is invalid (any of them <= 0)</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>{@code keywords} 为空或者空字符串</li>
+     *   <li>{@code pageSize} 和/或 {@code pageNum} 无效（任何一个 <= 0）</li>
      * </ul>
-     * If any of the corner case happened, {@code null} shall be returned.
+     * 如果出现任何边界情况，应该返回 {@code null}。
      */
     List<String> searchVideo(AuthInfo auth, String keywords, int pageSize, int pageNum);
 
     /**
-     * Calculates the average view rate of a video.
-     * The view rate is defined as the user's view time divided by the video's duration.
+     * 计算视频的平均观看率。
+     * 观看率定义为用户的观看时间除以视频的持续时间。
      *
-     * @param bv the video's {@code bv}
-     * @return the average view rate
-     * @apiNote You may consider the following corner cases:
+     * @param bv 视频的 {@code bv}
+     * @return 平均观看率
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>no one has watched this video</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>没有人观看过这个视频</li>
      * </ul>
-     * If any of the corner case happened, {@code -1} shall be returned.
+     * 如果出现任何边界情况，应该返回 {@code -1}。
      */
     double getAverageViewRate(String bv);
 
     /**
-     * Gets the hotspot of a video.
-     * With splitting the video into 10-second chunks, hotspots are defined as chunks with the most danmus.
+     * 获取视频的热点。
+     * 将视频分割成10秒钟的片段，热点定义为弹幕最多的片段。
      *
-     * @param bv the video's {@code bv}
-     * @return the index of hotspot chunks (start from 0)
-     * @apiNote You may consider the following corner cases:
+     * @param bv 视频的 {@code bv} 标识
+     * @return 热点片段的索引（从0开始）
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>no one has sent danmu on this video</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>没有人在这个视频上发送弹幕</li>
      * </ul>
-     * If any of the corner case happened, an empty set shall be returned.
+     * 如果发生任何边界情况，应返回一个空集合。
      */
     Set<Integer> getHotspot(String bv);
 
     /**
-     * Reviews a video by a superuser.
-     * If the video is already reviewed, do not modify the review info.
+     * 由超级用户审核视频。
+     * 如果视频已经被审核过，则不修改审核信息。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @return {@code true} if the video is newly successfully reviewed, {@code false} otherwise
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv} 标识
+     * @return 如果视频是新成功审核的，则返回 {@code true}，否则返回 {@code false}
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>{@code auth} is not a superuser or he/she is the owner</li>
-     *   <li>the video is already reviewed</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>{@code auth} 不是超级用户或他/她是视频所有者</li>
+     *   <li>视频已经被审核过</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果发生任何边界情况，应返回 {@code false}。
      */
+
     boolean reviewVideo(AuthInfo auth, String bv);
 
     /**
-     * Donates one coin to the video. A user can at most donate one coin to a video.
-     * The user can only coin a video if he/she can search it ({@link io.sustc.service.VideoService#searchVideo(AuthInfo, String, int, int)}).
-     * It is not mandatory that the user shall watch the video first before he/she donates coin to it.
+     * 给视频投币。用户对一个视频最多只能投一枚币。
+     * 用户只有在能搜索到该视频时（{@link io.sustc.service.VideoService#searchVideo(AuthInfo, String, int, int)}）才能给视频投币。
+     * 投币前不强制要求用户观看视频。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @return whether a coin is successfully donated
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv} 标识
+     * @return 是否成功投币
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>the user cannot search this video or he/she is the owner</li>
-     *   <li>the user has no coin or has donated a coin to this video</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>用户无法搜索到这个视频或他/她是视频所有者</li>
+     *   <li>用户没有币或已经对这个视频投过币</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果发生任何边界情况，应返回 {@code false}。
      */
+
+
     boolean coinVideo(AuthInfo auth, String bv);
 
     /**
-     * Likes a video.
-     * The user can only like a video if he/she can search it ({@link io.sustc.service.VideoService#searchVideo(AuthInfo, String, int, int)}).
-     * If the user already liked the video, the operation will cancel the like.
-     * It is not mandatory that the user shall watch the video first before he/she likes to it.
+     * 点赞视频。
+     * 用户只有在能搜索到该视频时才能点赞视频。
+     * 如果用户已经点赞了视频，该操作将取消点赞。
+     * 点赞前不强制要求用户观看视频。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @return the like state of the user to this video after this operation
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv} 标识
+     * @return 该操作后用户对此视频的点赞状态
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>the user cannot search this video or the user is the video owner</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>用户无法搜索到这个视频或用户是视频所有者</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果发生任何边界情况，应返回 {@code false}。
      */
+
     boolean likeVideo(AuthInfo auth, String bv);
 
     /**
-     * Collects a video.
-     * The user can only collect a video if he/she can search it.
-     * If the user already collected the video, the operation will cancel the collection.
-     * It is not mandatory that the user shall watch the video first before he/she collects coin to it.
+     * 收藏视频。
+     * 用户只有在能搜索到该视频时才能收藏视频。
+     * 如果用户已经收藏了视频，该操作将取消收藏。
+     * 收藏前不强制要求用户观看视频。
      *
-     * @param auth the current user's authentication information
-     * @param bv   the video's {@code bv}
-     * @return the collect state of the user to this video after this operation
-     * @apiNote You may consider the following corner cases:
+     * @param auth 当前用户的认证信息
+     * @param bv   视频的 {@code bv} 标识
+     * @return 该操作后用户对此视频的收藏状态
+     * @apiNote 您可能需要考虑以下边界情况：
      * <ul>
-     *   <li>{@code auth} is invalid, as stated in {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)}</li>
-     *   <li>cannot find a video corresponding to the {@code bv}</li>
-     *   <li>the user cannot search this video or the user is the video owner</li>
+     *   <li>{@code auth} 无效，如 {@link io.sustc.service.UserService#deleteAccount(AuthInfo, long)} 中所述</li>
+     *   <li>找不到对应 {@code bv} 的视频</li>
+     *   <li>用户无法搜索到这个视频或用户是视频所有者</li>
      * </ul>
-     * If any of the corner case happened, {@code false} shall be returned.
+     * 如果发生任何边界情况，应返回 {@code false}。
      */
+
     boolean collectVideo(AuthInfo auth, String bv);
 }
