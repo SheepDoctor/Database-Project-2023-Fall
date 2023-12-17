d65182ff1452433103e1b31b358d99b7206503d2
Bingwen Dong <757648641@qq.com>
2023-12-17 19:54:30 +0800

    recommend finished

--- sustc-api/src/main/java/io/sustc/dto/UserInfoResp.java
+++ sustc-api/src/main/java/io/sustc/dto/UserInfoResp.java
@@ -50,4 +50,84 @@ public class UserInfoResp {
      * The videos' {@code bv}s posted by this user.
      */
     private String[] posted;
+
+    public long getMid()
+    {
+        return mid;
+    }
+
+    public void setMid(long mid)
+    {
+        this.mid = mid;
+    }
+
+    public int getCoin()
+    {
+        return coin;
+    }
+
+    public void setCoin(int coin)
+    {
+        this.coin = coin;
+    }
+
+    public long[] getFollowing()
+    {
+        return following;
+    }
+
+    public void setFollowing(long[] following)
+    {
+        this.following = following;
+    }
+
+    public long[] getFollower()
+    {
+        return follower;
+    }
+
+    public void setFollower(long[] follower)
+    {
+        this.follower = follower;
+    }
+
+    public String[] getWatched()
+    {
+        return watched;
+    }
+
+    public void setWatched(String[] watched)
+    {
+        this.watched = watched;
+    }
+
+    public String[] getLiked()
+    {
+        return liked;
+    }
+
+    public void setLiked(String[] liked)
+    {
+        this.liked = liked;
+    }
+
+    public String[] getCollected()
+    {
+        return collected;
+    }
+
+    public void setCollected(String[] collected)
+    {
+        this.collected = collected;
+    }
+
+    public String[] getPosted()
+    {
+        return posted;
+    }
+
+    public void setPosted(String[] posted)
+    {
+        this.posted = posted;
+    }
 }
